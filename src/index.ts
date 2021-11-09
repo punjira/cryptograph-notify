9cf103de26b06e2f947c7e32e1cd744d4ffee866
punjira <saeedwmail@gmail.com>
2021-11-09 02:47:55 -0500

    change listening server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+import http from 'http';
+
+import { createIOClient } from './socket/socket';
 import { MongoConnect } from './database/mongo';
 import { createSubscriptions } from './nats/subscriptions';
 
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
 const app = express();
+
+app.use(cors());
+app.use(bodyParser({ extended: true }));
+
+const server = http.createServer(app);
+createIOClient(server);
 
 MongoConnect(() => {
      createSubscriptions();
 });
 
-app.listen(process.env.PORT, () => {});
+server.listen(process.env.PORT, () => {});
