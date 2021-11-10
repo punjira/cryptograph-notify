90b0d0e00d272c790c25e024357823c096157057
punjira <saeedwmail@gmail.com>
2021-11-10 06:34:23 -0500

    get and emit signal event

--- src/nats/subscriptions.ts
+++ src/nats/subscriptions.ts
@@ -6,6 +6,7 @@ import { Emitter } from '../socket/socket';
 const PRICE_UPDATE_EVENT = 'PRICE_UPDATE_EVENT';
 const CANDLE_UPDATE_EVENT = 'NEW_CANDLESTICK_UPDATE_EVENT';
 const FEED_UPDATE_EVENT = 'things';
+const NEW_SIGNAL_EVENT = 'NEW_SIGNAL_EVENT';
 
 export function createSubscriptions() {
      const client = natsClient.getInstance().getClient();
@@ -13,6 +14,16 @@ export function createSubscriptions() {
           const priceSubscriptions = client.subscribe(PRICE_UPDATE_EVENT);
           const candleSubscription = client.subscribe(CANDLE_UPDATE_EVENT);
           const feedSubscriptions = client.subscribe(FEED_UPDATE_EVENT);
+          const signalSubscription = client.subscribe(NEW_SIGNAL_EVENT);
+          signalSubscription.on('message', (message: Message) => {
+               const msg = message.getData();
+               if (typeof msg === 'string') {
+                    try {
+                         const parsed = JSON.parse(msg);
+                         Emitter('signal_update', parsed);
+                    } catch (err: any) {}
+               }
+          });
           priceSubscriptions.on('message', (message: Message) => {
                const msg = message.getData();
                if (typeof msg === 'string') {
