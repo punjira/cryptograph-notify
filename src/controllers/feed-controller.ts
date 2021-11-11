8a0293324661e36e87ed59b3341031223c6fed4d
punjira <saeedwmail@gmail.com>
2021-11-11 08:25:05 -0500

    add get controller

--- src/controllers/feed-controller.ts
+++ src/controllers/feed-controller.ts
@@ -1,6 +1,6 @@
 import { FeedModel, Feed } from '../models/feed-model';
 
-export function createFeed(params: Feed): Promise<any> {
+export function createFeed(params: Feed): Promise<Feed> {
      return new Promise((resolve, reject) => {
           FeedModel.create(params, function (err, result) {
                if (err) {
@@ -11,6 +11,21 @@ export function createFeed(params: Feed): Promise<any> {
      });
 }
 
-export function getPaginatedFeeds(req, res, next) {}
+export function getPaginatedFeeds(req, res, next) {
+     FeedModel.find({})
+          .sort({ date: -1 })
+          .limit(20)
+          .exec(function (err, result) {
+               if (err) {
+                    return res.status(500).json({
+                         data: err,
+                         message: 'something went wrong!',
+                    });
+               }
+               return res.status(200).json({
+                    data: result,
+               });
+          });
+}
 
 export function getAdminFeeds(req, res, next) {}
